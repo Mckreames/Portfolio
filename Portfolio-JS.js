8b09276cb783923dca18ed99dd8abd217915161f
Mckreames <114367512+Mckreames@users.noreply.github.com>
2023-12-09 01:13:31 -0500

    updated some information for more realistic applications

--- Portfolio-JS.js
+++ Portfolio-JS.js
@@ -1,12 +1,23 @@
 `use strict`;
 
+// Function to open the modal
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+// Function to close the modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+// Add a click event listener to the trigger element
+document.getElementById("overlay-icon-L").addEventListener("click", openModal);
+
 let pigGameBtn = document.querySelector(`.pig-game-web-link`);
 
 pigGameBtn.addEventListener(`click`, function () {
   alert(`This app isn't published due to it just being a tutorial I followed.`);
 });
-
-
 
 // JavaScript function to format the phone number
 function formatPhoneNumber() {
@@ -14,12 +25,12 @@ function formatPhoneNumber() {
   var phoneNumberInput = document.getElementById("phoneNumber");
 
   // Get the input value and remove non-numeric characters
-  var phoneNumber = phoneNumberInput.value.replace(/\D/g, '');
+  var phoneNumber = phoneNumberInput.value.replace(/\D/g, "");
 
   // Check if the phone number is not empty
   if (phoneNumber.length > 0) {
     // Format the phone number as (XXX) XXX-XXXX
-    phoneNumber = phoneNumber.replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3');
+    phoneNumber = phoneNumber.replace(/(\d{3})(\d{3})(\d{4})/, "($1) $2-$3");
 
     // Set the formatted value back to the input
     phoneNumberInput.value = phoneNumber;
@@ -27,4 +38,6 @@ function formatPhoneNumber() {
 }
 
 // Attach the formatPhoneNumber function to the input's blur event
-document.getElementById("phoneNumber").addEventListener("blur", formatPhoneNumber);
+document
+  .getElementById("phoneNumber")
+  .addEventListener("blur", formatPhoneNumber);
