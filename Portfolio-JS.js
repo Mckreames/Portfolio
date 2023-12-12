e53a761088035cc87849010d68c1fe613085f03d
Mckreames <114367512+Mckreames@users.noreply.github.com>
2023-12-12 03:03:24 -0500

    Just screwed up the modal window... crud. gotta leave it till later

--- Portfolio-JS.js
+++ Portfolio-JS.js
@@ -38,3 +38,36 @@ hideElements.forEach((el) => observer.observe(el));
 
 const slowHideElements = document.querySelectorAll(".slow-hide");
 slowHideElements.forEach((el) => observer.observe(el));
+
+// Modal Windows
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnClosedModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+console.log(btnsOpenModal);
+
+const openModal = function () {
+  modal.classList.remove(`hidden`);
+  overlay.classList.remove(`hidden`);
+};
+
+const closeModal = function () {
+  modal.classList.add(`hidden`);
+  overlay.classList.add(`hidden`);
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener(`click`, openModal);
+  // modal.style.display = `block`;
+}
+
+btnClosedModal.addEventListener(`click`, closeModal);
+overlay.addEventListener(`click`, closeModal);
+
+document.addEventListener(`keydown`, function (e) {
+  console.log(e.key);
+
+  if (e.key === `Escape` && !modal.classList.contains(`hidden`)) {
+    closeModal();
+  }
+});
