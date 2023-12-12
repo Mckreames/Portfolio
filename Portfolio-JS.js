0f14d310793a9b6f989ec3060e54195059419306
Mckreames <114367512+Mckreames@users.noreply.github.com>
2023-12-12 00:31:57 -0500

    added in slide transitions to some elements

--- Portfolio-JS.js
+++ Portfolio-JS.js
@@ -41,3 +41,19 @@ function formatPhoneNumber() {
 document
   .getElementById("phoneNumber")
   .addEventListener("blur", formatPhoneNumber);
+
+// Element Fade In
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("slide");
+    }
+  });
+});
+
+const hideElements = document.querySelectorAll(".hide");
+hideElements.forEach((el) => observer.observe(el));
+
+const slowHideElements = document.querySelectorAll(".slow-hide");
+slowHideElements.forEach((el) => observer.observe(el));
